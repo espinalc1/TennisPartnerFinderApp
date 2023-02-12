02f50b48478b461cba8ed25798b0fede4ef377c0
xlstc110 <307468086@qq.com>
2023-02-12 12:39:50 -0800

    Home page

---
+++ Frontend/tennis-front/src/components/CreateAccount.js
@@ -0,0 +1,84 @@
+import React, {Component} from 'react';
+
+class CreateAccount extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            username : "",
+            password : "",
+            gender: "",
+            age: 0,
+            ustaRating: 0
+        }
+    }
+
+    usernameHandler = (event) => {
+        this.setState({
+            username: event.target.value
+        });
+    }
+
+    passwordHandler = (event) => {
+        this.setState(
+            {
+                password: event.target.value
+            }
+        );
+    }
+
+    genderHandler = (event) => {
+        this.setState(
+            {
+                gender: event.target.value
+            }
+        );
+    }
+
+    ageHandler = (event) => {
+        this.setState(
+            {
+                age: event.target.value
+            }
+        );
+    }
+
+    ustaRatingHandler = (event) => {
+        this.setState(
+            {
+                ustaRating: event.target.value
+            }
+        );
+    }
+
+    submitHandler = () => {
+
+    }
+
+    render() {
+        return(
+        <div>
+            <form>
+            <div>
+                <h1>Please enter information for your account</h1>
+                <label>Username</label>
+                <input type="text" value={this.state.username} onChange={this.usernameHandler}></input>
+                <label>Password</label>
+                <input type="password" value={this.state.password} onChange={this.passwordHandler}></input>
+                <label>Gender</label>
+                <select>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                </select>
+                <label>Age</label>
+                <input type="number" value={this.state.age} onChange={this.ageHandler}></input>
+                <label>USTA Rating</label>
+                <input type="number" value={this.state.ustaRating} onChange={this.ustaRatingHandler}></input>
+                <button type="submit">Create account</button>
+            </div>
+            </form>
+        </div>) 
+        
+    }
+}
+
+export default CreateAccount;
