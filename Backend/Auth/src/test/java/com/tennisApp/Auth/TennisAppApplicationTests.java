4923717e7b03d1800a21302947594a3feb90f023
espinalc1 <christopher.espinal@gmail.com>
2023-02-13 12:25:30 -0700

    Auth MS

---
+++ Backend/Auth/src/test/java/com/tennisApp/Auth/TennisAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tennisApp.Auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
